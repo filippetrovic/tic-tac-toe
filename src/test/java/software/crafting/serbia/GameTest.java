ab3fe862c438a934be0a25d7ba856ca62da5706a
Filip Petrovic <fpetrovic92@gmail.com>
2019-11-10 13:22:56 +0100

    Retrofit test

--- src/test/java/software/crafting/serbia/GameTest.java
+++ src/test/java/software/crafting/serbia/GameTest.java
@@ -4,52 +4,55 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class GameTest {
 
-  private static final Player PLAYER_X = new Player('X');
-  private static final Player PLAYER_O = new Player('O');
+  private static final Player PLAYER = mock(Player.class);
 
   @Mock
   private Board board;
 
+  @Mock
+  private PlayerSequence sequence;
+
+  @InjectMocks
   private Game game;
 
   @BeforeEach
   void setUp() {
-    game = new Game(board, new StandardSequence());
+    when(sequence.current())
+        .thenReturn(PLAYER);
   }
 
   @Test
-  @DisplayName("Should add player X's move to board when the board is empty")
-  void shouldAddPlayerXMoveToBoardWhenBoardIsEmpty() {
+  @DisplayName("Should invoke the player to take a move")
+  void shouldInvokePlayerToTakeAMove() {
 
     // When
     game.takeMove(0, 0);
 
     // Then
-    verify(board).addMove(new Move(0, 0, PLAYER_X));
+    verify(PLAYER).takeMoveOn(0, 0, board);
 
   }
 
   @Test
-  @DisplayName("Should alternate players after each move")
-  void shouldAddAlternatePlayersAfterEachMove() {
+  @DisplayName("Should notify that round is done")
+  void shouldNotifyThatRoundIsDone() {
 
     // When
     game.takeMove(0, 0);
-    game.takeMove(1, 0);
-    game.takeMove(1, 2);
 
     // Then
-    verify(board).addMove(new Move(0, 0, PLAYER_X));
-    verify(board).addMove(new Move(0, 1, PLAYER_O));
-    verify(board).addMove(new Move(2, 1, PLAYER_X));
+    verify(sequence).next();
 
   }
 
